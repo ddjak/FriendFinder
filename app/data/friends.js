9a7618f51fc3d7982657d6b29e3fbba310869ad6
admin <admin>
2017-10-16 01:07:05 -0400

    almost final

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [
+  {
+    "name":"Hot Head Henry",
+    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMSVLKMGtBM1kUKKbDTgXfvpNEK0I45UBfRUmeAHyEsruJDd2A",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  },{
+  "name" : "Modern Rock Bob",
+  "photo" : "http://www.pizzafm.org/wp-content/uploads/2016/10/classic-rock.jpg",
+  "scores" : [1,1,1,1,1,1,1,1,1,1]
+  }, {
+    "name" : "Jeff",
+    "photo" : "http://www.ikea.com/PIAimages/0121010_PE277826_S5.JPG",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+  }];
+
+module.exports = friends;
